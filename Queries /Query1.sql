bd62eba6412fc3c47fe5d64bd599a57738404019
Divye Bajaj <129332407+DivyeB@users.noreply.github.com>
2025-06-27 19:44:18 +0530

    Update Query1.sql

--- Queries /Query1.sql
+++ Queries /Query1.sql
@@ -2,8 +2,8 @@
 SELECT viewer_id
 FROM (
   SELECT viewer_id,
-         MAX(login_date) AS last_login
+         MAX(DATE(login_date)) AS last_login_date
   FROM `ott.Logins`
   GROUP BY viewer_id
 ) AS last_logins
-WHERE last_login < '2024-03-31' - INTERVAL 3 MONTH;
+WHERE last_login_date < DATE_SUB(DATE('2024-03-31'), INTERVAL 3 MONTH);
