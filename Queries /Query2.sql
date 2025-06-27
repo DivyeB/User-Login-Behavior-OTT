e66404cc59f0f0b8c59f3f017f28f7a4ac39b7fd
Divye Bajaj <129332407+DivyeB@users.noreply.github.com>
2025-06-27 19:56:22 +0530

    Delete Queries /Query2.sql

--- Queries /Query2.sql
+++
@@ -1,6 +0,0 @@
--- What's the DAU (Daily Active Users) trend?
-SELECT DATE(login_date) AS login_day,
-       COUNT(DISTINCT viewer_id) AS Daily_Active_Users
-FROM `ott.Logins`
-GROUP BY login_day
-ORDER BY login_day;
