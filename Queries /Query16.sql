cf0d71e5f85b569692fae7b99e395c59f540deab
Divye Bajaj <129332407+DivyeB@users.noreply.github.com>
2025-06-27 19:58:01 +0530

    Delete Queries  directory

--- Queries /Query16.sql
+++
@@ -1,8 +0,0 @@
--- Peak login days across regions?
-SELECT viewer_id,login_date
-FROM (
-  SELECT viewer_id,login_date,
-         ROW_NUMBER() OVER (PARTITION BY viewer_id ORDER BY login_date DESC) AS rn
-  FROM ott.Logins
-) AS ranked
-WHERE rn = 1;
